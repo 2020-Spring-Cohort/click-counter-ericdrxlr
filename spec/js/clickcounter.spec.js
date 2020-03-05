d77ba834fc3507fba8a9920fa4c8bcf87f211735
Eric Drexler <ericdrxlr@gmail.com>
2020-03-05 12:16:08 -0500

    more tests, i will be more specific

--- spec/js/clickcounter.spec.js
+++ spec/js/clickcounter.spec.js
@@ -20,12 +20,51 @@ describe ('Click Counter', function()
             expect(sut.getUsersClicks()).toBe(0)
         });
     })
-    describe('clickincrementor', function()
+    describe('ClickIncrementor', function()
     {
         it('increases value of usersClicks by 1', function()
         {
-            sut.clickIncrementor()
+            sut.clickIncrementor()   //Act
             expect(sut.usersClicks).toBe(1)
         });
     })
+    describe('get click companion count', function()
+    {
+        it('returns value of 0', function()
+        {
+            expect(sut.getClickCompanionCount()).toBe(0)
+        });
+    })
+    describe('click companion value', function()
+    {
+        it('returns cost of 100', function()
+        {
+            expect(sut.getClickCompanionValue()).toBe(100);
+        });
+    })
+    describe('purchase clicking companion', function()
+    {
+        it('increases click companion count to 1', function()
+        {
+            sut.purchaseClickCompanion()
+            expect(sut.clickCompanionCount).toBe(1)
+        });
+    })
+    describe('purchase clicking companion', function()
+    {
+        it('decreases users clicks by 100', function()
+        {
+            sut.purchaseClickCompanion()
+            expect(sut.clickCompanionCount).toBe(1)
+        });
+    })
+    describe('purchasing clicking companions', function()
+    {
+        it('increases companion value by 10%', function()
+        {
+            sut.purchaseClickCompanion()
+            expect(sut.clickCompanionValue).toBe(110)
+        });
+    })
+   
 }) 
--- src/js/clickcounter.js
+++ src/js/clickcounter.js
@@ -3,7 +3,10 @@ class ClickCounter
     constructor()
     {
         this.usersClicks = 0;
-    }
+        this.clickCompanionCount = 0;
+        this.clickCompanionValue = 100;
+        
+    }//methods below
     getUsersClicks()
     {
         return this.usersClicks;
@@ -12,4 +15,24 @@ class ClickCounter
     {
         this.usersClicks++;
     }
+    getClickCompanionCount()
+    {
+        return this.clickCompanionCount;
+    }
+    getClickCompanionValue()
+    {
+        return this.clickCompanionValue;
+    }
+    purchaseFurtherClickCompanions()
+    {
+        clickCompanionValue = clickCompanionValue*1.1;
+    } 
+    purchaseClickCompanion()
+    {
+        this.clickCompanionCount++;
+        this.usersClicks =- 100;
+        this.purchaseFurtherClickCompanions;   
+    } 
+   
+
 }
